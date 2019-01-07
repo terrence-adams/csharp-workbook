24290bcd6a366dee8ced7cb245dfb574db92f095
Terrence Adams <terrence.adams@gmail.com>
2019-01-07 01:56:52 -0600

    Saying Hello to Myself updated

--- HelloWorld/HelloWorld assignment 1.cs
+++ HelloWorld/HelloWorld assignment 1.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hell <your name> ! ");
+            Console.WriteLine("Hello <your name> ! ");
         }
     }
 }
