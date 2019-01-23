d199780927933cff04a179ccbae986e62540bb0f
Terrence Adams <terrence.adams@gmail.com>
2019-01-23 01:27:04 -0600

    RPS

---
+++ Sandbox/TicTacToe/TicTacToe.cs
@@ -0,0 +1,166 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        public static string x = "x";
+        public static string y = "y";
+        public static int row = 0;
+        public static int column = 0;
+
+        public static bool gameWon = false;
+
+        public static string playerTurn = "x";
+        public static string[][] board = new string[][]
+        {
+            new string[] {" ", " ", " "},
+            new string[] {" ", " ", " "},
+            new string[] {" ", " ", " "}
+        };
+
+        public static void Main()
+        {
+            do
+            {
+                DrawBoard();
+                GetInput();
+                placeMarkForPlayer();
+                
+
+            } while (!CheckForWin() && !CheckForTie());
+
+            // leave this command at the end so your program does not close automatically
+            Console.ReadLine();
+        }
+
+        public static void GetInput()
+        {
+            
+            Console.WriteLine("Player " + playerTurn);
+            Console.WriteLine("Enter Row:");
+            row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Column:");
+            column = int.Parse(Console.ReadLine());
+            
+
+
+        }
+
+        public static void placeMarkForPlayer()
+        {
+            
+            PlaceMark();
+            setTogglePlayerTurn();
+
+
+        }
+
+
+        public static void setTogglePlayerTurn()
+        {
+           playerTurn = (playerTurn == x) ? y : x;
+
+        }
+
+      
+        public static void PlaceMark()
+        {   
+            board[row][column] = playerTurn;
+        }
+
+        public static bool CheckForWin()
+        {
+            
+            if(HorizontalWin() || VerticalWin () || DiagonalWin() )
+            {
+                gameWon = true;
+                return gameWon;
+               // Console.WriteLine("Player {0}", playerTurn + " Won!!! ");
+            }
+
+            else
+            {
+             return gameWon;  
+            }
+
+            
+        }
+
+        public static bool CheckForTie()
+        {
+
+            return gameWon;
+        }
+        
+        public static bool HorizontalWin()
+        {
+        bool win = false;
+
+        for(int i = 0; i < 3; i++)
+        {
+            if(board[i][0] == playerTurn && board[i][1] == playerTurn && board[i][2] == playerTurn)
+            {
+                win = true;
+                return win;
+                
+            }
+
+        }
+
+
+        return win;
+        }
+
+        public static bool VerticalWin()
+        {
+            bool win = false;
+
+            
+        for(int i = 0; i < 3; i++)
+        {
+            if(board[0][i] == playerTurn && board[1][i] == playerTurn && board[2][i] == playerTurn)
+            {
+                win = true;
+                return win;
+                
+            }
+
+        }
+
+            return win;
+        }
+
+        public static bool DiagonalWin()
+        {
+            bool win = false;
+
+            if(board[0][0] == playerTurn && board[1][1] == playerTurn && board[2][2]== playerTurn)
+            {
+                win = true;
+                return win;
+
+            }
+
+             else if(board[2][0] == playerTurn && board[1][1] == playerTurn && board[0][2]== playerTurn)
+            {
+                win = true;
+                return win;
+
+            }
+            
+
+            return win;
+        }
+
+        public static void DrawBoard()
+        {
+            Console.WriteLine("  0 1 2");
+            Console.WriteLine("0 " + String.Join("|", board[0]));
+            Console.WriteLine("  -----");
+            Console.WriteLine("1 " + String.Join("|", board[1]));
+            Console.WriteLine("  -----");
+            Console.WriteLine("2 " + String.Join("|", board[2]));
+        }
+    }
+}
