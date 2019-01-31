da21c8952117ee639bd0940ea2efc20fecda1e21
Terrence Adams <terrence.adams@gmail.com>
2019-01-30 22:04:45 -0600

    Added bubble sort

---
+++ Sort/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int[] myArray = {2,33,45,12,456,23,22,90,0,100};
+            int temp = 0;
+
+            for(int i = 0 ; i < myArray.Length -1 ; i++)
+                for( int j = 0; j < myArray.Length -1; j++)
+                    if(myArray[j] > myArray[j+1])
+                    {
+                        temp = myArray[j+1];
+                        myArray[j+1] = myArray[j];
+                        myArray[j] = temp;
+
+                    }
+
+
+
+            foreach(int i in myArray)
+            {
+                Console.WriteLine(i.ToString());
+
+            }
+
+
+        }
+    }
+}
