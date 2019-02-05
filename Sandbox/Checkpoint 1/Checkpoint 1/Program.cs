efcbcbc49c2a4553965c70a5d1429af2ccff8e17
Terrence Adams <terrence.adams@gmail.com>
2019-02-04 21:09:03 -0600

    modified files

---
+++ Sandbox/Checkpoint 1/Checkpoint 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Checkpoint_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckPointClassOne cp1 = new CheckPointClassOne();
+            cp1.divisbleBYThree();
+            cp1.sumOfAll();
+            cp1.getFactorialFactory();
+            cp1.guessingGame();
+            cp1.findTheHighestNumber();
+        }
+    }
+}
