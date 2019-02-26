6f70f46c5c5faa0596e4893f734a46932ca48c65
Batman <terrence.adams@gmail.com>
2019-02-25 21:06:09 -0600

    updated class placement

--- Sandbox/LINQTA/Student.cs
+++ Sandbox/LINQTA/Student.cs
@@ -3,21 +3,21 @@ using System;
 namespace LINQTA
 {
 
-    public class Student
-	{
-		public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Address { get; set; }
-		public int Balance { get; set; }
+    // public class Student
+	// {
+	// 	public string Name { get; set; }
+    //     public string Phone { get; set; }
+    //     public string Address { get; set; }
+	// 	public int Balance { get; set; }
 		
-		public Student (string name, string phone, string address, int balance)
-		{
-			Name = name;
-			Phone = phone;
-			Address = address;
-			Balance = balance;
-		}
-	}
+	// 	public Student (string name, string phone, string address, int balance)
+	// 	{
+	// 		Name = name;
+	// 		Phone = phone;
+	// 		Address = address;
+	// 		Balance = balance;
+	// 	}
+	// }
 
 
 
