6324e67b91f417bca2577e34cd55a5624138cdc9
Batman <terrence.adams@gmail.com>
2019-02-25 19:28:33 -0600

    Enums

---
+++ Sandbox/Enums/Enums.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Enums
+{
+    public class Enums
+    {
+
+        public enum Day {Monday, Tuesday, Wednesday,Thursday, Friday, Saturday, Sunday };
+        public enum Month {January, February,March, April, MarshalByRefObject, June, July, August, September, October, November, December};
+
+
+    }
+
+
+}
---
+++ Sandbox/Enums/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+           var myValues = Enum.GetValues(typeof(Enums.Day));
+           foreach(var v in myValues) 
+           {
+            Console.WriteLine(v);
+           }
+        }
+    }
+}
