89abd48c487bc5b2475a24c531172849790713d8
Batman <terrence.adams@gmail.com>
2019-02-25 20:58:51 -0600

    LingStudent Statement

---
+++ Sandbox/LINQTA/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace LINQTA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+       
+		List<Student> students = new List<Student>();
+		
+		students.Add(new Student("Chris", "123-456-7891", "123 Delany", -2990));
+		students.Add(new Student("Kevin", "512-222-2222", "435 Carolyn", -2500));
+		students.Add(new Student("Victoria", "512-827-8498", "701 Brazos St", 0));
+		students.Add(new Student("Luke", "555-555-5555", "451 Brody Ln", -500));
+		
+       IEnumerable <Student> negBalance =
+        from student in students
+        where student.Balance < 0
+        select student;
+
+		foreach(Student s in negBalance)
+        {
+            Console.WriteLine(s.Balance);
+        }
+		
+        }
+    }
+}
---
+++ Sandbox/LINQTA/Student.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace LINQTA
+{
+
+    public class Student
+	{
+		public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+		public int Balance { get; set; }
+		
+		public Student (string name, string phone, string address, int balance)
+		{
+			Name = name;
+			Phone = phone;
+			Address = address;
+			Balance = balance;
+		}
+	}
+
+
+
+
+}
