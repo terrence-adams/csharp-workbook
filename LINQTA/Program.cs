4508ae6d5af501cb0df4bb360e2cdbde73cfdd33
Batman <terrence.adams@gmail.com>
2019-02-25 21:08:56 -0600

    moved project to parent folder after changes

--- LINQTA/Program.cs
+++ LINQTA/Program.cs
@@ -11,23 +11,39 @@ namespace LINQTA
         {
             
        
-		List<Student> students = new List<Student>();
-		
-		students.Add(new Student("Chris", "123-456-7891", "123 Delany", -2990));
-		students.Add(new Student("Kevin", "512-222-2222", "435 Carolyn", -2500));
-		students.Add(new Student("Victoria", "512-827-8498", "701 Brazos St", 0));
-		students.Add(new Student("Luke", "555-555-5555", "451 Brody Ln", -500));
-		
-       IEnumerable <Student> negBalance =
-        from student in students
-        where student.Balance < 0
-        select student;
+            List<Student> students = new List<Student>();
+            
+            students.Add(new Student("Chris", "123-456-7891", "123 Delany", -2990));
+            students.Add(new Student("Kevin", "512-222-2222", "435 Carolyn", -2500));
+            students.Add(new Student("Victoria", "512-827-8498", "701 Brazos St", 0));
+            students.Add(new Student("Luke", "555-555-5555", "451 Brody Ln", -500));
+            
+        IEnumerable <Student> negBalance =
+            from student in students
+            where student.Balance < 0
+            select student;
 
-		foreach(Student s in negBalance)
-        {
-            Console.WriteLine(s.Balance);
-        }
+            foreach(Student s in negBalance)
+            {
+                Console.WriteLine(s.Balance);
+            }
 		
         }
     }
+
+     public class Student
+	{
+		public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+		public int Balance { get; set; }
+		
+		public Student (string name, string phone, string address, int balance)
+		{
+			Name = name;
+			Phone = phone;
+			Address = address;
+			Balance = balance;
+		}
+	}
 }
