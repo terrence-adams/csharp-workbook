1a012c206bf9ab8250aa1358bea6da38ae9715be
Batman <terrence.adams@gmail.com>
2019-02-25 21:02:23 -0600

    moved to parent folder

---
+++ Structs/MyStructs.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Windows;
+using System.Drawing;
+
+namespace Structs
+{
+   public struct pointStruct
+    {
+        public int x ;
+        public int y;
+
+        public pointStruct(int myX, int myY)
+        {
+            pointStruct.x = myX;
+            pointStruct.y = myY;
+
+
+        }
+
+
+    }
+
+
+
+    public class MyPoint
+    {
+        Point point1 = new Point(3, 5);
+        Point point2 = new Point(4 , 3);
+
+       pointStruct myPS = new pointStruct(5,5);
+
+       
+
+      
+
+            
+
+
+    }
+}
