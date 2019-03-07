b87823ff64462c361cda9721dda18276c0124382
Terrence Adams <terrence.adams@gmail.com>
2019-03-06 22:55:03 -0600

    tweaked file handling.

--- Sandbox/filehandling2/FileHandling.cs
+++ Sandbox/filehandling2/FileHandling.cs
@@ -7,26 +7,28 @@ namespace filehandling2
 {
     class FileHandling
     {
-        
-        
+
+
         private string msg1 = "This is a text file.";
         private string msg2 = "This is a text file, and I can edit it.";
         public string createFile(string myFileName)
         {
             string txt = ".txt";
             string fullFileName = myFileName + txt;
-            string myPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            
+            string myPath = ".";
+            //Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+
             string fullPath = $"{myPath}\\{fullFileName}";
-            
 
-           using(FileStream fs1 =   File.Create(fullPath))
-           using(StreamWriter sw = new StreamWriter(fs1))
-           {
 
-               sw.WriteLine(msg1);
+            using (FileStream fs1 = File.Create(fullPath))
+            using (StreamWriter sw = new StreamWriter(fs1))
+            {
 
-           }
+                sw.WriteLine(msg1);
+
+            }
 
             Console.WriteLine("Done.");
             return fullPath;
@@ -39,12 +41,12 @@ namespace filehandling2
         {
 
 
-            using(FileStream f1 = File.OpenWrite(path))
-                using( StreamWriter sw1 = new StreamWriter(f1))
-                {
-                    sw1.WriteLine(msg2);
+            using (FileStream f1 = File.OpenWrite(path))
+            using (StreamWriter sw1 = new StreamWriter(f1))
+            {
+                sw1.WriteLine(msg2);
 
-                }
+            }
             Console.WriteLine("The file at {0} was edited.", path);
 
             return path;
@@ -53,24 +55,42 @@ namespace filehandling2
 
         public void numberOfWords(string filePath)
         {
-            string [] wordsArray;
+            string[] wordsArray;
             wordsArray = File.ReadAllLines(filePath, Encoding.UTF8);
             int count = wordsArray.Length;
-            Console.WriteLine("Total count of words in the file is.{0}", count);
-            
+            Console.WriteLine("Total count of words in the file is {0}.", count);
+
+        }
+
+        public void numberChars(string filePath)
+        {
+            string[] wordsArray;
+            wordsArray = File.ReadAllLines(filePath, Encoding.UTF8);
+            int count = 0;
+            foreach(string s in wordsArray)
+            {
+                foreach(char c in s)
+                {
+                    count++;
+                }
+
+            }
+
+            Console.WriteLine("There are {0} characters in this file.", count);
+
         }
 
 
         public void longestWord(string filePath)
         {
-            var file = File.ReadAllLines (filePath,Encoding.UTF8);
-            var wordList = new List <string>(file);
+            var file = File.ReadAllLines(filePath, Encoding.UTF8);
+            var wordList = new List<string>(file);
             string longestWord = "";
             int longest = 0;
 
-            foreach( string s in wordList)
+            foreach (string s in wordList)
             {
-                if(s.Length > longest)
+                if (s.Length > longest)
                 {
                     longestWord = s;
                     longest = s.Length;
@@ -78,7 +98,7 @@ namespace filehandling2
 
             }
 
-             Console.WriteLine("The longest word is {0}.",longestWord);
+            Console.WriteLine("The longest word is {0}.", longestWord);
 
         }
 
@@ -92,7 +112,7 @@ namespace filehandling2
 
         }
 
-        
+
 
     }
 }
--- Sandbox/filehandling2/Program.cs
+++ Sandbox/filehandling2/Program.cs
@@ -6,7 +6,16 @@ namespace filehandling2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string scrabblePath = ".\\scrabble.txt";
+            FileHandling fh = new FileHandling();
+            string filePath = fh.createFile("test1");
+            fh.editFile(filePath);
+            string filePath2 = fh.createFile("test2");
+            fh.removeFile(filePath2);
+            fh.longestWord(scrabblePath);
+            fh.numberOfWords(scrabblePath);
+            fh.numberChars(scrabblePath);
+
         }
     }
 }
